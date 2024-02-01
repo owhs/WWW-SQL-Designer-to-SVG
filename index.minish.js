866ae6c079b484c19ebb8a6c209ee48705cceff9
owhs <46844122+owhs@users.noreply.github.com>
2024-02-01 11:15:29 +0000

    Update index.minish.js

--- index.minish.js
+++ index.minish.js
@@ -1,10 +1,11 @@
-((_NS = "http://www.w3.org/2000/svg") => {
+((D=document,_NS = "http://www.w3.org/2000/svg",W="width",H="height",L="left",q="querySelector",Q="querySelectorAll") => {
 
     var _round = x => +(+x).toFixed(1),
-        E = dn=>document.createElementNS(_NS, dn),
-        A = (el,att,v)=>el.setAttribute(att,v),
+        E = dn=>D.createElementNS(_NS, dn),
+        A = (el,att,v,R)=>el.setAttribute(att,R ? _round(v) : v),
+        O = (el,o)=>el["offset"+o[0].toUpperCase()+o.slice(1)],
         AP = (el,w)=>el.append(w),
-        svg = document.querySelector("#area>svg").cloneNode(1),
+        svg = D[q]("#area>svg").cloneNode(1),
         style = E("style");
 
     A(svg,"xmlns", _NS);
@@ -15,79 +16,77 @@
     svg.prepend(style);
 
 
-    document.querySelectorAll("#area>.table").forEach(el => {
+    D[Q]("#area>.table").forEach(el => {
         var g = E("g"),
             rect = E("rect"),
-            ELL = _round(el.offsetLeft),
-            ELT = _round(el.offsetTop),
-            ELW = _round(el.offsetWidth);
+            ELL = O(el,"left"),
+            ELT = O(el,"top"),
+            ELW = O(el,W);
 
         
-        A(rect,"x", ELL);
-        A(rect,"y", ELT);
-        A(rect,"width", ELW);
-        A(rect,"height", _round(el.offsetHeight));
+        A(rect,"x", ELL, 1);
+        A(rect,"y", ELT, 1);
+        A(rect,W, ELW, 1);
+        A(rect,H, O(el,H), 1);
 
 
         AP(g,rect);
 
-        [...el.querySelectorAll("tr")].forEach((t, i) => {
+        [...el[Q]("tr")].forEach((t, i) => {
             var rg = E("g"),
                 text = E("text"),
                 v = t.innerText.trim(),
                 r = E("rect"),
                 r2 = E("rect"),
-                TT = t.offsetTop,
-                TH = t.offsetHeight;
+                TT = O(t,"top"),
+                TH = O(t,H);
 
-            A(r,"x", _round(ELL + ELW - 10));
-            A(r,"y", _round(ELT + TT));
-            A(r,"width", 4);
-            A(r,"height", _round(TH));
+            A(r,"x", (ELL + ELW - 10), 1);
+            A(r,"y", (ELT + TT), 1);
+            A(r,W, 4);
+            A(r,H, TH, 1);
 
-            A(r2,"width", 8);
-            A(r2,"height", 8);
+            A(r2,W,8);
+            A(r2,H,8);
             A(r2,"fill", "#7db2dd");
 
-            if (t.querySelector(".primary,.key")) text.classList.add("b");
+            if (t[q](".primary,.key")) text.classList.add("b");
 
 
             text.innerHTML = v;
 
             AP(rg,text);
             AP(g,rg);
+            var title;
 
             if (i === 0) {
-                var title = t.querySelector("td").title;
+                title = t[q]("td").title;
                 if (title) {
-                    A(r2,"x", _round(ELL + ELW - 8));
-                    A(r2,"y", _round(ELT + TT));
-
-                    var tt = E("title");
-                    tt.innerHTML = title;
-                    AP(rg,r2);
-                    AP(rg,tt);
+                    A(r2,"x", (ELL + ELW - 8), 1);
+                    A(r2,"y", (ELT + TT), 1);
                 }
 
-                A(text,"x", _round(ELL + (ELW / 2) - (+(v.length * 6.2) / 2)));
-                A(text,"y", _round(ELT + TT + 18));
+                A(text,"x", (ELL + (ELW / 2) - (+(v.length * 6.2) / 2)), 1);
+                A(text,"y", (ELT + TT + 18), 1);
             } else {
-                var p = t.parentElement,color = p.style.borderColor.slice(4, -1).split(", ").map(x => (x ? +x : "").toString(16)).join(""),
-                    title = p.title;
+                var p = t.parentElement,color = p.style.borderColor.slice(4, -1).split(", ").map(x => (x ? +x : "").toString(16)).join("");
+                title = p.title;
                 if (color) {
                     A(r,"fill", "#" + color);
                     AP(rg,r)
                 }
                 if (title) {
-                    A(r2,"x", _round(ELL + ELW - 16));
-                    A(r2,"y", _round(ELT + TT + ((TH - 8) / 2)));
-                    var tt = E("title");
-                    tt.innerHTML = title;
-                    AP(rg,r2);
-                    AP(rg,tt);
+                    A(r2,"x", (ELL + ELW - 16), 1);
+                    A(r2,"y", (ELT + TT + ((TH - 8) / 2)), 1);
                 }
-                A(text,"x", _round(ELL + t.offsetLeft + 10));
-                A(text,"y", _round(ELT + TT + 15));
+                A(text,"x", (ELL + O(t,"left") + 10), 1);
+                A(text,"y", (ELT + TT + 15), 1);
+            }
+            if (title) {
+                var tt = E("title");
+                tt.innerHTML = title;
+                AP(rg,r2);
+                AP(rg,tt);
             }
 
         });
