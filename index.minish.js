ecf714dbd32cdcc0010ca7145e2205e19f6bb58c
owhs <46844122+owhs@users.noreply.github.com>
2024-02-01 11:30:39 +0000

    Update index.minish.js

--- index.minish.js
+++ index.minish.js
@@ -1,25 +1,25 @@
-((D=document,_NS = "http://www.w3.org/2000/svg",W="width",H="height",L="left",q="querySelector",Q="querySelectorAll") => {
+((D=document,_NS = "http://www.w3.org/2000/svg",S="style",R="rect",s="slice",f="fill",W="width",H="height",L="left",q="querySelector",Q="querySelectorAll",F="forEach",T="title",I="innerHTML") => {
 
     var _round = x => +(+x).toFixed(1),
         E = dn=>D.createElementNS(_NS, dn),
         A = (el,att,v,R)=>el.setAttribute(att,R ? _round(v) : v),
-        O = (el,o)=>el["offset"+o[0].toUpperCase()+o.slice(1)],
+        O = (el,o)=>el["offset"+o[0].toUpperCase()+o[s](1)],
         AP = (el,w)=>el.append(w),
         svg = D[q]("#area>svg").cloneNode(1),
-        style = E("style");
+        style = E(S);
 
     A(svg,"xmlns", _NS);
     A(svg,"font-family", "verdana, sans-serif");
     A(svg,"font-size", "12");
 
-    style.innerHTML = "text{cursor:default}.b{font-weight:bold}g>rect:first-child{fill:#f4f4f4}";
+    style[I] = "text{cursor:default}.b{font-weight:bold}g>rect:first-child{fill:#f4f4f4}";
     svg.prepend(style);
 
 
-    D[Q]("#area>.table").forEach(el => {
+    D[Q]("#area>.table")[F](el => {
         var g = E("g"),
-            rect = E("rect"),
-            ELL = O(el,"left"),
+            rect = E(R),
+            ELL = O(el,L),
             ELT = O(el,"top"),
             ELW = O(el,W);
 
@@ -32,12 +32,12 @@
 
         AP(g,rect);
 
-        [...el[Q]("tr")].forEach((t, i) => {
+        [...el[Q]("tr")][F]((t, i) => {
             var rg = E("g"),
                 text = E("text"),
                 v = t.innerText.trim(),
-                r = E("rect"),
-                r2 = E("rect"),
+                r = E(R),
+                r2 = E(R),
                 TT = O(t,"top"),
                 TH = O(t,H);
 
@@ -48,19 +48,19 @@
 
             A(r2,W,8);
             A(r2,H,8);
-            A(r2,"fill", "#7db2dd");
+            A(r2,f, "#7db2dd");
 
             if (t[q](".primary,.key")) text.classList.add("b");
 
 
-            text.innerHTML = v;
+            text[I] = v;
 
             AP(rg,text);
             AP(g,rg);
             var title;
 
             if (i === 0) {
-                title = t[q]("td").title;
+                title = t[q]("td")[T];
                 if (title) {
                     A(r2,"x", (ELL + ELW - 8), 1);
                     A(r2,"y", (ELT + TT), 1);
@@ -69,22 +69,22 @@
                 A(text,"x", (ELL + (ELW / 2) - (+(v.length * 6.2) / 2)), 1);
                 A(text,"y", (ELT + TT + 18), 1);
             } else {
-                var p = t.parentElement,color = p.style.borderColor.slice(4, -1).split(", ").map(x => (x ? +x : "").toString(16)).join("");
-                title = p.title;
+                var p = t.parentElement,color = p[S].borderColor[s](4, -1).split(", ").map(x => (x ? +x : "").toString(16)).join("");
+                title = p[T];
                 if (color) {
-                    A(r,"fill", "#" + color);
+                    A(r,f, "#" + color);
                     AP(rg,r)
                 }
                 if (title) {
                     A(r2,"x", (ELL + ELW - 16), 1);
                     A(r2,"y", (ELT + TT + ((TH - 8) / 2)), 1);
                 }
-                A(text,"x", (ELL + O(t,"left") + 10), 1);
+                A(text,"x", (ELL + O(t,L) + 10), 1);
                 A(text,"y", (ELT + TT + 15), 1);
             }
             if (title) {
-                var tt = E("title");
-                tt.innerHTML = title;
+                var tt = E(T);
+                tt[I] = title;
                 AP(rg,r2);
                 AP(rg,tt);
             }
